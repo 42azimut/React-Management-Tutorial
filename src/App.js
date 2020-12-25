a2de6d16ff4ba2f1ec237bed79162c1d69117e5e
42azimut <azimutuniverse@gmail.com>
2020-12-26 04:00:12 +0900

    materail ui lecture-06

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
 import React, { Component } from 'react';
 import Customer from './components/Customer'
 import './App.css';
+import Paper from '@material-ui/core/Paper';
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = theme => ({
+  root: {
+    width: '100%',
+    marginTop: theme.spacing.unit * 3,
+    overflowX: "auto"
+  },
+  table: {
+    minWidth: 1080
+  }
+})
 
 const customers = [
   {
-  'id': 1,
-  'image': 'https://placeimg.com/64/64/3',
-  'name': 'Jayden',
-  'birthday': '961222',
-  'gender': '남자',
-  'job': '대학생'
+    'id': 1,
+    'image': 'https://placeimg.com/64/64/3',
+    'name': 'Jayden',
+    'birthday': '961222',
+    'gender': '남자',
+    'job': '대학생'
   },
   {
     'id': 2,
@@ -18,27 +36,40 @@ const customers = [
     'birthday': '760315',
     'gender': '여자',
     'job': '대학원생'
-    },
-    {
-      'id': 3,
-      'image': 'https://placeimg.com/64/64/2',
-      'name': '배수지',
-      'birthday': '561002',
-      'gender': '남자',
-      'job': '교수'
-      }
+  },
+  {
+    'id': 3,
+    'image': 'https://placeimg.com/64/64/2',
+    'name': '배수지',
+    'birthday': '561002',
+    'gender': '남자',
+    'job': '교수'
+    }
 ]
 
 
 class App extends Component {
   render() {
+    const { classes } = this.props;
     return (
-      <div>
-        {
+      <Paper className={classes.root}>
+        <Table className={classes.table}>
+          <TableHead>
+            <TableRow>
+              <TableCell>번호</TableCell>
+              <TableCell>이미지</TableCell>
+              <TableCell>이름</TableCell>
+              <TableCell>생년월일</TableCell>
+              <TableCell>성별</TableCell>
+              <TableCell>직업</TableCell>
+            </TableRow>
+          </TableHead>
+        <TableBody>
+        { 
           customers.map(c_one => {
             return (
               <Customer
-                key={c_one.id}
+                key={c_one.id}  // 중요! 키 설정할것!
                 id={c_one.id}
                 image={c_one.image}
                 name={c_one.name}
@@ -49,9 +80,11 @@ class App extends Component {
             );
           })
         }
-      </div>
+         </TableBody>
+        </Table>
+      </Paper>
     );
   }
 }
 
-export default App;
+export default withStyles(styles)(App);
